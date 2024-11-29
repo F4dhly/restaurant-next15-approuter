4dd9c5642c6eadc2af11d038140e332ae90ac308
fadhly <fadhlyajiput@gmail.com>
2024-11-29 21:38:09 +0700

    Init: Initialize folder project
    Start project with Next JS 15 and tailwind css

---
+++ app/about/page.js
@@ -0,0 +1,7 @@
+export default function AboutPage() {
+    return (
+        <main className="text-center">
+            <h1>About us</h1>
+        </main>
+    );
+}
--- app/layout.js
+++ app/layout.js
@@ -19,9 +19,9 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className="h-full">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased h-full m-0 bg-[#525B44] text-slate-100 text`}
       >
         {children}
       </body>
